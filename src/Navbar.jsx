a20a68c312fd5bf3fa901127b3f42bb7d0370ee6
MidlajN <midlaj4n@gmail.com>
2024-03-23 20:47:24 +0530

    Logo Added

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,17 +1,18 @@
-
+import  logo  from '/logonew.svg';
 
 export default function Navbar() {
     return (
         <>
            <div className="flex w-full items-center justify-between navbar h-[9%]">
-               <div className="heading">
+               <div className="heading flex items-center justify-center gap-2">
+                   <img src={logo} className='w-7' alt="logo"/>
                    <span className="svg">SVG</span><span className="editor">Editor</span>
                </div>
-               <div className="github">
+               {/* <div className="github">
                    <a href="https://github.com/MidlajN/react-svg-editor" target="_blank">
                        <i className="fa-brands fa-github fa-2xl"></i>
                     </a>
-               </div>
+               </div> */}
            </div>
         </>
     )
